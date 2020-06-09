266e0e940dd62d3f3e68ea41720870275b711e15
dianaAltair <dsanabrianieto@altair.com>
2020-06-09 15:03:29 -0400

    chore: include babel for jsx and React

--- fullstack_template/static/webpack.config.js
+++ fullstack_template/static/webpack.config.js
@@ -9,6 +9,13 @@ const config = {
     resolve: {
         extensions: ['.js', '.jsx', '.css']
     },
+    module: {
+        rules: [{
+            test: /\.jsx?/,
+            exclude: /node_modules/,
+            use: 'babel-loader'
+        }]
+    }
 };
 
 module.exports = config;
